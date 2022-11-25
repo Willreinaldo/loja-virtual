31a08e00fd2c57890b9ab435d4f42e81e0fc338b
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2022-11-25 16:47:02 -0300

    first commit

---
+++ src/lojaVirtual/TestaConexao.java
@@ -0,0 +1,16 @@
+package lojaVirtual;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "williammonteiro10");
+		  System.out.println("Fechando conexão!!!");
+
+	       connection.close();
+	}
+
+}
